2d8b96caa64342ff4ebffd6f42659181947b0606
Shaad98 <shaadbangi98@gmail.com>
2026-06-08 21:04:34 +0530

    feat: upload file to existing S3 bucket using Terraform

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -11,3 +11,10 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-bucket-xyz928"
 
 }
+
+resource "aws_s3_object" "my_text_file" {
+
+  source = "./program.txt"
+  key = "main.txt"
+  bucket = aws_s3_bucket.my_bucket.bucket
+}
